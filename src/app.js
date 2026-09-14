17bc38f764d67a4eeec189944e34b5fd2aba75d4
Simran Singh <simransingh631716@gmail.com>
2026-09-14 11:40:18 +0530

    done

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 const app=express();
 app.use(cors({
     origin:process.env.app_URL,
-    credemtials:true
+    credentials:true
 }));
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true}))
