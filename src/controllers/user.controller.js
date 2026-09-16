fcb3213415eaa83d7465dded8e76f4572e478e7d
Simran Singh <simransingh631716@gmail.com>
2026-09-17 01:16:48 +0530

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,45 @@
 import {asynchandler} from "../utils/asyncHandler.js";
+import apierror from "../utils/Apierror.js";
+import {user} from "..models/user.model.js";
+
 
  const registeruser = asynchandler(async (req,res) =>{
    res.status(200).json({
     messsage:"register user"
    })
+   const {username,email,password,fullname}=req.body
+   console.log("email",email);
  })
+ if(fullname ==""){
+  throw new apierror(400,"fullname is required ")
+ }
+ //here i can use this condition on every field name by using an array and using some method 
+if(
+  [fullname,email,username,password].some((field)=> 
+    field?.trim() === ""))
+  {
+      throw new apierror(400,"all the fields should not be empty ")
+   }
+const existed =user.findOne(
+  {  
+    $or: [{username},{email}]
+  }
+)
+ if(existed){
+  throw apierror(409, "User already exists ")
+ }
 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 export {registeruser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import{Router} from "express";
 import {registeruser} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 const router = Router();
 
+//here we are inserting middleware 
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
 
-router.route("/register").post(registeruser)
+    registeruser)
+    
 export default router;
 
