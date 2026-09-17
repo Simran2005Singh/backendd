5dfb6133ecd958be515c0a228c29e3bd07e4eaa2
Simran Singh <simransingh631716@gmail.com>
2026-09-17 22:40:37 +0530

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,15 @@
 import {asynchandler} from "../utils/asyncHandler.js";
 import apierror from "../utils/Apierror.js";
 import {user} from "..models/user.model.js";
-
-
+import {apiresponse} from "..utils/apiresponse.js";
+import {uploadOnCloudinary} from "../utils/cloudinary"
  const registeruser = asynchandler(async (req,res) =>{
    res.status(200).json({
     messsage:"register user"
    })
    const {username,email,password,fullname}=req.body
    console.log("email",email);
- })
+ 
  if(fullname ==""){
   throw new apierror(400,"fullname is required ")
  }
@@ -17,7 +17,7 @@ import {user} from "..models/user.model.js";
 if(
   [fullname,email,username,password].some((field)=> 
     field?.trim() === ""))
-  {
+   {
       throw new apierror(400,"all the fields should not be empty ")
    }
 const existed =user.findOne(
@@ -28,18 +28,37 @@ const existed =user.findOne(
  if(existed){
   throw apierror(409, "User already exists ")
  }
+//this is localpath as the files are not uploaded in cloudinary 
+const localpath =req.files?.avatar[0]?.path ;
+const coverimagelocalpath =req.files?.coverImage[0]?.path;
+if(!localpath){
+  throw new apierror(400, "Avatar is required ")
+}
 
+const avatar= await uploadOnCloudinary(localpath)
+const coverimage= await uploadOnCloudinary(coverimagelocalpath)
+if(!avatar){
+  throw new apierror(400,"Avatar is reqired")
+}
+const user =user.create({
+  fullname,
+  avatar : avatar.url,
+  coverimage :coverimage?.url || "",
+  email,
+  password,
+  username:username.toLowerCase()
+})
 
+ const usercreated = await user.findById(user._id).select(
+  "-password -refreshtoken"
+ )
+ if(!usercreated){
+  throw new apierror(500,"user not created")
+ }
 
+return res.status(201).json(
+  new apiresponse(200,usercreated,"user is registered")
+)
+})
 
-
-
-
-
-
-
-
-
-
- 
 export {registeruser}
